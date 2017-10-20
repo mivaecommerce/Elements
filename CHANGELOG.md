1f590352965055574497b973e343bbbea4165878
Matt Zimmermann <matt@influxwebdesign.com>
2017-10-20 14:37:39 -0700

    Elements v1.0.0
    This is the initial, public release of the Elements Framework for the Miva ecommerce platform.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,14 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## 1.0.0 - 2017-10-20
+### Added
+- Theme Files `mm5/themes/elements/*`
+
+### Changed
+- Updated Framework PKG
+- Updates to Page Templates
+
 ## 0.2.0 - 2017-10-19
 ### Added
 - Additional Documentation and Page Templates
---
+++ mm5/themes/elements/ui/js/theme.js
@@ -0,0 +1,35 @@
+/**
+ * Use this file to all in scripts and functions you would like to have globally or on specific
+ * pages. You will use this to extend your theme instead of adding code to the core framework files.
+ */
+
+var themeFunctionality = {
+	init: function () {
+		/*$.loadScript(theme_path + 'extensions/mini-basket/mini-basket.js', function () {
+			miniBasket.init();
+		});*/
+	},
+	jsSFNT: function() {
+	},
+	jsCTGY: function() {
+	},
+	jsPROD: function() {
+		//$.loadScript(theme_path + 'extensions/product-layout/ajax-add-to-cart.js');
+	},
+	jsPLST: function() {
+	},
+	jsSRCH: function() {
+	},
+	jsBASK: function() {
+	},
+	jsORDL: function() {
+	},
+	jsOCST: function() {
+	},
+	jsOSEL: function() {
+	},
+	jsOPAY: function() {
+	},
+	jsINVC: function() {
+	}
+};
